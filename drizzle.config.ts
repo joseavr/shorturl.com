270338e7bad200b5ed08770266e845ab3e2facb0
lamula21 <jvaldiv8@montgomerycollege.edu>
2025-06-26 03:27:28 -0400

    added drizzle with turso

---
+++ drizzle.config.ts
@@ -0,0 +1,24 @@
+import { config } from "dotenv"
+import { defineConfig } from "drizzle-kit"
+
+config({ path: ".env" })
+
+// Configuration file used ONLY by Drizzle Kit CLI,
+// not used for database operations.
+// Can have multiple drizzle.config.ts files.
+// By default, the CLI will look for `drizzle.config.ts` file,
+// otherwise specify the path with --config=...
+// i.e: drizzle-kit generate --config=drizzle-dev.config.ts
+export default defineConfig({
+	dialect: "turso",
+	schema: "./src/database/drizzle/schemas/*",
+	out: "./src/database/drizzle/migrations",
+	dbCredentials: {
+		url: process.env.TURSO_DATABASE_URL as string,
+		authToken: process.env.TURSO_AUTH_TOKEN || undefined
+	},
+	migrations: {
+		// migration files name are created by timestamp instead of by index
+		prefix: "timestamp"
+	}
+})
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 import type { NextConfig } from "next";
 
 /** Allow us to use bindings during local development */
-if (process.env.NODE_ENV === 'development') await setupDevPlatform();
+if (process.env.NODE_ENV === 'development') setupDevPlatform();
 
 const nextConfig: NextConfig = {
   /* config options here */
