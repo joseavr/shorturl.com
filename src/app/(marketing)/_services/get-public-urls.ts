57b228746782ad3ff0fd4b44ae5e7aa1b2c65243
lamula21 <jvaldiv8@montgomerycollege.edu>
2025-07-08 21:12:05 -0400

    replaced hardcoded hostname with environment variable

--- src/app/(marketing)/_services/get-public-urls.ts
+++ src/app/(marketing)/_services/get-public-urls.ts
@@ -2,12 +2,12 @@ import z from "zod"
 import { SelectPublicUrlSchema } from "@/backend/database/drizzle/schemas"
 import { onSuccessResponseSchema } from "@/backend/utils/http-response-factory"
 
-const HOST_NAME = "http://localhost:3000"
+const APP_URL = process.env.NEXT_PUBLIC_APP_URL
 const result = onSuccessResponseSchema(z.array(SelectPublicUrlSchema))
 type UrlsResponse = z.infer<typeof result>
 
 export async function getPlublicUrls() {
-	const response = await fetch(`${HOST_NAME}/api/urls/public`)
+	const response = await fetch(`${APP_URL}/api/urls/public`)
 	const json: UrlsResponse = await response.json()
 	const urls = json.success ? json.data : []
 
