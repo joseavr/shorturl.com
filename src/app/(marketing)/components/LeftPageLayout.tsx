b558069c6363d54ee2ccc12664ef118a1336fdb0
joseavr <jvaldiv8@montgomerycollege.edu>
2025-07-20 05:56:38 -0400

    implement conditional rendering of dashboard access or account creation

--- src/app/(marketing)/components/LeftPageLayout.tsx
+++ src/app/(marketing)/components/LeftPageLayout.tsx
@@ -1,10 +1,13 @@
 import { IconWrapper } from "@subframe/core"
 import Link from "next/link"
+import { getServerSessionCache } from "@/features/auth/lib/session"
 import { Button } from "@/ui"
 import { GoogleIcon } from "@/ui/icons/google"
 import { PostUrlForm } from "./PostUrlForm"
 
-export function LeftPageLayout() {
+export async function LeftPageLayout() {
+	const { isAuthenticated } = await getServerSessionCache()
+
 	return (
 		<div className="flex shrink-0 grow basis-0 flex-col items-center justify-center gap-6 self-stretch px-12 py-12">
 			<div className="flex w-full max-w-[448px] flex-col items-start justify-center gap-8">
@@ -32,22 +35,35 @@ export function LeftPageLayout() {
 					<div className="flex h-px shrink-0 grow basis-0 flex-col items-center gap-2 bg-neutral-border" />
 				</div>
 				<div className="flex w-full flex-col items-center gap-2">
-					<Button
-						asChild
-						className="h-10 w-full flex-none"
-						variant="brand-secondary"
-						size="large"
-					>
-						<Link href="/api/auth/google">
-							Create an account with{" "}
-							<IconWrapper>
-								<GoogleIcon />
-							</IconWrapper>
-						</Link>
-					</Button>
-					<span className="font-body text-body text-subtext-color">
-						Get access to private links and detailed analytics
-					</span>
+					{isAuthenticated ? (
+						<Button
+							asChild
+							className="h-10 w-full flex-none"
+							variant="brand-secondary"
+							size="large"
+						>
+							<Link href="/dashboard">Access your Dashboard</Link>
+						</Button>
+					) : (
+						<>
+							<Button
+								asChild
+								className="h-10 w-full flex-none"
+								variant="brand-secondary"
+								size="large"
+							>
+								<Link href="/api/auth/google">
+									Create an account with{" "}
+									<IconWrapper>
+										<GoogleIcon />
+									</IconWrapper>
+								</Link>
+							</Button>
+							<span className="font-body text-body text-subtext-color">
+								Get access to private links and detailed analytics
+							</span>
+						</>
+					)}
 				</div>
 			</div>
 		</div>
