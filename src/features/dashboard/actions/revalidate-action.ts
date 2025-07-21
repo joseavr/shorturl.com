f031b1d896c91fd749129e07922aa811a01417bc
joseavr <jvaldiv8@montgomerycollege.edu>
2025-07-21 04:25:01 -0400

    add help function

--- src/features/dashboard/actions/revalidate-action.ts
+++
@@ -1,7 +0,0 @@
-/** biome-ignore-all lint/suspicious/noConfusingVoidType: It is fine here */
-"use server"
-import { revalidatePath } from "next/cache"
-
-export async function revalidateAction(path: string) {
-	revalidatePath(path)
-}
---
+++ src/utils/delay.ts
@@ -0,0 +1,2 @@
+export const delay = (duration: number, callback: () => void) =>
+	setTimeout(callback, duration)
