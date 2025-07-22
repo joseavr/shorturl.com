1c43cfb1bb28d30a2159efd2f109533c00a01a8d
joseavr <jvaldiv8@montgomerycollege.edu>
2025-07-22 18:02:39 -0400

    flip flag to true to show shorturlanalyticspage

--- src/flags.ts
+++ src/flags.ts
@@ -1,4 +1,4 @@
 import { isDev } from "./const"
 
 export const TableMenuActionsFG = isDev || true
-export const showShorturlAnalyticsPage = isDev || false
+export const showShorturlAnalyticsPage = isDev || true
