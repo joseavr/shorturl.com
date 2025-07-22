c3c4c044da96639384e277028400a14c9d88b22d
joseavr <jvaldiv8@montgomerycollege.edu>
2025-07-22 01:28:00 -0400

    add custom fields in headers
    - must store custom data in headers because cannot access request object in page.tsx nextjs

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,5 @@
 export const isDev = process.env.NODE_ENV === "development"
 export const appUrl = process.env.NEXT_PUBLIC_APP_URL as string
+export const ipHeaderName = "x-client-ip"
+export const geoHeaderName = "x-client-city"
+export const referrerHeaderName = "x-referrer"
